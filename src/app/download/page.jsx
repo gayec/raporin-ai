146588ce9133d237d1cbb52245ff47192c381e7b
“firatulgay” <firat.ulg@gmail.com>
2026-08-31 23:30:04 +0300

    url change

--- src/app/download/page.jsx
+++ src/app/download/page.jsx
@@ -51,7 +51,7 @@ export default function DownloadPage() {
         {/* Gradient Buton */}
         {/* Gradient Buton */}
         <a
-          href="https://downloads.raporin.com/RaporinAI-1.1.19.msi"
+          href="https://downloads.raporin.com/RaporinAI-1.1.21.msi"
           onClick={handleDownloadClick}
           className="inline-flex items-center gap-2 bg-gradient-to-r from-[#17C6A3] to-[#0F918B] text-white font-semibold px-8 py-4 rounded-full shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-300 transform hover:-translate-y-1"
         >
